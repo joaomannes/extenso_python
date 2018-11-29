4202b0c64299e46399f918378edfa8688b0de218
joaogm25 <joaogm@joaogm.com.br>
2018-11-29 20:55:29 -0200

    Adicionado número máximo

--- extenso.py
+++ extenso.py
@@ -9,6 +9,9 @@ class Palavra:
 class Extenso:
 
     def __init__(self):
+
+        self._numero_maximo = 999999999999999999999999999999999999999999999
+
         # Dicionários para armazenar os números por extenso
         self.unidades = {1: 'um', 2: 'dois', 3: 'três', 4: 'quatro', 5: 'cinco', 6: 'seis', 7: 'sete', 8: 'oito', 9: 'nove', 10 : 'dez', 
                          11 : 'onze', 12 : 'doze', 13 : 'treze', 14 : 'quatorze', 15 : 'quinze', 16 : 'dezesseis', 17 : 'dezessete', 18 : 'dezoito', 19 : 'dezenove'}
@@ -26,6 +29,8 @@ class Extenso:
 
 
     def escrever(self, numero):
+        if (numero > self._numero_maximo):
+            raise Exception('Número informado maior que o número máximo suportado')
         extenso = ''
 
         # Conversão do número recebido para string
